6f75cef97d1b05e2bc6660c54e6c1e18d5f2bd03
YugeTen <jimmyshi820@gmail.com>
2021-06-23 23:35:13 +0100

    add update function to add items to original flags using dict

--- forge/experiment_tools.py
+++ forge/experiment_tools.py
@@ -313,6 +313,12 @@ def _restore_flags(flags):
     _flags.FLAGS.__dict__['__parsed'] = True
 
 
+def update(flags_dict):
+    """Add new flags specified by flags_dict."""
+    for k, v in flags_dict.items():
+        _flags.FLAGS.__setattr__(k, v)
+
+
 def print_flags():
     """Pretty-prints config flags."""
     flags = _flags.FLAGS.__flags
